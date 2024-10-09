c30de56e3ffed31ea03aa1d25b7c34c7e3ae259e
Psybah <abiodunoluwamurewa@gmail.com>
2024-10-09 16:43:47 +0100

    70% done with the header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          blue: '#1c3076'
+      },
+    },
   },
   plugins: [],
 }
