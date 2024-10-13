013a3b713a5b8213fee251589945914bbb4c4a19
Psybah <abiodunoluwamurewa@gmail.com>
2024-10-13 01:57:46 +0100

    pulling changes

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,6 +6,7 @@ export default function RegisterPage() {
       <div className="mb-64">
       <h1 className="text-4xl text-center mb-4">Register</h1>
       <form className="max-w-md mx-auto">
+        <input type="text" placeholder="Full name" />
         <input type="email" placeholder="your@email.com"/>
         <input type="password" placeholder="password"/>
         <button className="primary">SignUp</button>
