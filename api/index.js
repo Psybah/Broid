82ae0400089034b34df12788cd1d2cf3359cdd3b
Psybah <abiodunoluwamurewa@gmail.com>
2024-10-14 20:13:19 +0100

    login  & register authentication complete with database setup, alo along password encryption enabled

--- api/index.js
+++ api/index.js
@@ -5,11 +5,13 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const User = require('./models/User.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const helmet = require('helmet');
 const path = require('path');
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'agawerhga';
 
 const PORT = process.env.PORT || 4000;
 mongoose.connect(process.env.MONGODB_URL);
@@ -52,7 +54,25 @@ app.post('/register', async (req, res) => {
       res.status(422).json(e);
     }
     
+});
 
+app.post('/login', async (req,res) => {
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json('pass ok');
+      });
+      res.cookie('token', '').json('pass ok');
+    } else{
+      res.status(422).json('pass not ok');
+    }
+  } else{
+    res.json('not found');
+  }
 });
 
 app.listen(4000, () => {
