659173eea5916f9ddd4519dfdf84837a6533d03a
Psybah <abiodunoluwamurewa@gmail.com>
2024-10-15 18:29:32 +0100

    authentication still

--- api/index.js
+++ api/index.js
@@ -64,9 +64,8 @@ app.post('/login', async (req,res) => {
     if (passOk) {
       jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err,token) => {
         if (err) throw err;
-        res.cookie('token', token).json('pass ok');
+        res.cookie('token', token).json(userDoc);
       });
-      res.cookie('token', '').json('pass ok');
     } else{
       res.status(422).json('pass not ok');
     }
