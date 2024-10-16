fc29c6d3e7de19fd1d516c860db80afce47cf180
Psybah <abiodunoluwamurewa@gmail.com>
2024-10-16 23:11:15 +0100

    made api calls appropriately

--- api/index.js
+++ api/index.js
@@ -40,56 +40,6 @@ app.get('/test', (req, res) => {
     res.send('Server is working');
   });
 
-app.post('/register', async (req, res) => {
-    const {name,email,password} = req.body;
-
-    try {
-      const userDoc = await User.create({
-        name,
-        email,
-        password:bcrypt.hashSync(password, bcryptSalt),
-      });
-      res.json(userDoc);
-    } catch (e) {
-      res.status(422).json(e);
-    }
-    
-});
-
-app.post('/login', async (req,res) => {
-  const {email,password} = req.body;
-  const userDoc = await User.findOne({email});
-  if (userDoc) {
-    const passOk = bcrypt.compareSync(password, userDoc.password);
-    if (passOk) {
-      jwt.sign({
-        email:userDoc.email, 
-        id:userDoc._id,
-      }, jwtSecret, {}, (err,token) => {
-        if (err) throw err;
-        res.cookie('token', token).json(userDoc);
-      });
-    } else{
-      res.status(422).json('pass not ok');
-    }
-  } else{
-    res.json('not found');
-  }
-});
-
-app.get('/profile', (req,res) => {
-  const {token} = req.cookies;
-  if (token) {
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) throw err;
-      const {name,email,_id} = await User.findById(userData.id);
-      res.json({name,email,_id});
-    })
-  } else {
-    res.json(null);
-  }
-  res.json({token});
-});
 
 app.listen(4000, () => {
     console.log('Server running on port 4000');
