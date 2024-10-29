a854af361535455382465f268e6745bc0df91f8b
Joe Black <oniyajoeolaiya@gmail.com>
2024-10-29 03:36:12 +0100

    Added some clarity to the embroideryRoutes and imageUploadRoutes files

--- api/routes/embroideryRoutes.js
+++ api/routes/embroideryRoutes.js
@@ -1,12 +1,43 @@
+// Importing necessary modules and controllers
 const express = require('express');
 const {createEmbroidery, getUserEmbroideries, getEmbroideryById, updateUserEmbroidery, getAllEmbroideries} = require('../controllers/embroideryController');
 
+// Ebroidery Routes
 const embroideryRouter = express.Router();
 
+/**
+ * @route POST /embroidery
+ * @desc Create a new embroidery design
+ * @access Admin or Authorized users
+ */
 embroideryRouter.post('/embroidery', createEmbroidery);
+
+/**
+ * @route GET /embroidery
+ * @desc Get all embroideries associated with the current user
+ * @access Protected
+ */
 embroideryRouter.get('/embroidery', getUserEmbroideries);
+
+/**
+ * @route GET /embroidery-by-id
+ * @desc Fetch embroidery details by ID
+ * @access Public or Protected (based on authentication)
+ */
 embroideryRouter.get('/embroidery-by-id', getEmbroideryById);
+
+/**
+ * @route PUT /update-embroidery
+ * @desc Update an existing embroidery design
+ * @access Admin or Authorized users
+ */
 embroideryRouter.put('/update-embroidery', updateUserEmbroidery);
+
+/**
+ * @route GET /embroideries
+ * @desc Get all embroidery designs
+ * @access Public or Protected
+ */
 embroideryRouter.get('/embroideries', getAllEmbroideries);
 
 module.exports = embroideryRouter;
--- api/routes/imageUploadRoutes.js
+++ api/routes/imageUploadRoutes.js
@@ -1,3 +1,4 @@
+// Importing necessary modules and controllers
 const express = require('express');
 const { uploadPhotos, uploadImageUsingLink } = require('../controllers/imageController');
 const multer = require("multer");
@@ -5,7 +6,18 @@ const multer = require("multer");
 const imageUploadRouter = express.Router();
 const imageMiddleware = multer({ dest: 'uploads/' });
 
+/**
+ * @route POST /upload-using-link
+ * @desc Upload an image using a link
+ * @access Public or Protected
+ */
 imageUploadRouter.post('/upload-using-link', uploadImageUsingLink);
+
+/**
+ * @route POST /upload
+ * @desc Upload one or more images
+ * @access Public or Protected
+ */
 imageUploadRouter.post('/upload', imageMiddleware.array('file', 100), uploadPhotos);
 
 module.exports = imageUploadRouter;
