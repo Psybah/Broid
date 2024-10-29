a48ad6cbad6b879d87bf7ced28793cc37c7d9def
Joe Black <oniyajoeolaiya@gmail.com>
2024-10-29 12:35:54 +0100

    Added some comments to the User model for more clarity.

--- api/models/User.js
+++ api/models/User.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// Define the User schema to represent a user account
 const UserSchema = new Schema({
     name: String,
+
+    // Unique email address for the user; used as their login identifier
     email: {type: String, unique: true},
+
     password: String,
 });
 
+// Create and export the User model based on the User schema
 const UserModel = mongoose.model('User', UserSchema);
 
 module.exports = UserModel;
