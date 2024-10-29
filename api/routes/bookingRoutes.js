ee352434e4c0d36adbf5388c8c6af72c7ec136e3
Joe Black <oniyajoeolaiya@gmail.com>
2024-10-29 03:16:06 +0100

    Added some description to the booking routes.

--- api/routes/bookingRoutes.js
+++ api/routes/bookingRoutes.js
@@ -1,9 +1,22 @@
+// Importing booking modules and controllers
 const express = require('express');
 const {bookEmbroidery, getUserBookings} = require('../controllers/bookingController');
 
+// Booking Routes
 const bookingRouter = express.Router();
 
+/**
+ * @route POST /bookings
+ * @desc Book an embroidery design
+ * @access Public or Protected (based on your authentication setup)
+ */
 bookingRouter.post('/bookings', bookEmbroidery);
+
+/**
+ * @route GET /bookings
+ * @desc Get all bookings for the current user
+ * @access Protected
+ */
 bookingRouter.get('/bookings', getUserBookings);
 
 module.exports = bookingRouter;
