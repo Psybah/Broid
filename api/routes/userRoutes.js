ef77f489cbe8670da90e594ac573239907e24b42
Joe Black <oniyajoeolaiya@gmail.com>
2024-10-29 03:41:23 +0100

    Added some comments to the userRoutes file

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,11 +1,35 @@
+// Importing User Authentication and Profile Routes
 const express = require('express');
 const {registerUser, loginUser, getProfile, logoutUser} = require('../controllers/userController');
 
 const userRouter = express.Router();
 
+/**
+ * @route POST /register
+ * @desc Register a new user
+ * @access Public
+ */
 userRouter.post('/register', registerUser);
+
+/**
+ * @route POST /login
+ * @desc Authenticate user and return a token
+ * @access Public
+ */
 userRouter.post('/login', loginUser);
+
+/**
+ * @route GET /profile
+ * @desc Get the profile of the logged-in user
+ * @access Protected
+ */
 userRouter.get('/profile', getProfile);
+
+/**
+ * @route GET /logout
+ * @desc Logout the current user
+ * @access Protected
+ */
 userRouter.get('/logout', logoutUser);
 
 module.exports = userRouter;
